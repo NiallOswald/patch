bdecec64d8451b1f723fead9b42496ccb62cc047
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-02-01 22:40:06 +0000

    Feat: Add time trial timer

--- cope/cli/__init__.py
+++ cope/cli/__init__.py
@@ -2,11 +2,12 @@
 
 import typer
 
-from cope.cli import auth, timetrial
+from cope.cli import auth, timetrial, timer
 
 app = typer.Typer()
 app.add_typer(auth.app, name="auth")
 app.add_typer(timetrial.app, name="timetrial")
+app.add_typer(timer.app, name="timer")
 
 if __name__ == "__main__":
     app()
