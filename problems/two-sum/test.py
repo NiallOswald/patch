0ea4ab0e70ec53836f79d7fa63f8e52f2da27b3e
Eleanor Kneip <eleanor.kneip2705@gmail.com>
2025-02-02 03:35:44 +0000

    Feat: two-sum problem set up

--- problems/two-sum/test.py
+++ problems/two-sum/test.py
@@ -7,9 +7,9 @@ class TestCase(unittest.TestCase):
         solution = skeleton.Solution()
         self.assertRaises(TypeError, solution.two_sum, None, None)
         self.assertRaises(ValueError, solution.two_sum, [], 0)
-        target = 7
-        nums = [1, 3, 2, -7, 5]
-        expected = [2, 4]
+        target = 6
+        nums = [3, 3, 4, 5]
+        expected = [0, 1]
         self.assertEqual(solution.two_sum(nums, target), expected)
         print("Success: test_two_sum")
 
