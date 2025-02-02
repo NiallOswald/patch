7f6e926b4199af0e126df375da1e846a81c7cd3e
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-02-02 08:33:14 +0000

    Feat: Add scoring CLI

---
+++ patch/cli/highscore.py
@@ -0,0 +1,27 @@
+"""Highscore commands."""
+
+import typer
+
+from patch.utils import ur
+
+app = typer.Typer()
+
+
+@app.callback(invoke_without_command=True)
+def highscore():
+    """Show the user's highscore."""
+    response = ur.score()
+    if not ur.has_error(response):
+        print(response.json())
+
+
+@app.command()
+def top():
+    """Show the highscores."""
+    response = ur.leaderboard()
+    if not ur.has_error(response):
+        print(response.json())
+
+
+if __name__ == "__main__":
+    app()
