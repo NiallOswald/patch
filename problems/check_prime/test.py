a831a77b1dc046d0dca05d801e189b567d29c88a
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-02-02 08:30:21 +0000

    Test: Refactor tests

--- problems/check_prime/test.py
+++ problems/check_prime/test.py
@@ -1,24 +1,22 @@
+import unittest, skeleton
 
-# %load test_check_prime.py
-import unittest
-from skeleton import *
 
-class TestMath(unittest.TestCase):
+class TestCase(unittest.TestCase):
 
     def test_check_prime(self):
-        math = Math()
+        math = skeleton.Math()
         self.assertRaises(TypeError, math.check_prime, None)
         self.assertRaises(TypeError, math.check_prime, 98.6)
         self.assertEqual(math.check_prime(0), False)
         self.assertEqual(math.check_prime(1), False)
         self.assertEqual(math.check_prime(97), True)
-        print('Success: test_check_prime')
+        print("Success: test_check_prime")
 
 
 def main():
-    test = TestMath()
+    test = TestCase()
     test.test_check_prime()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- problems/two-sum/test.py
+++ problems/two-sum/test.py
@@ -1,24 +1,23 @@
-# %load test_two_sum.py
-import unittest
-from skeleton import *
+import unittest, skeleton
 
-class TestTwoSum(unittest.TestCase):
+
+class TestCase(unittest.TestCase):
 
     def test_two_sum(self):
-        solution = Solution()
+        solution = skeleton.Solution()
         self.assertRaises(TypeError, solution.two_sum, None, None)
         self.assertRaises(ValueError, solution.two_sum, [], 0)
         target = 7
         nums = [1, 3, 2, -7, 5]
         expected = [2, 4]
         self.assertEqual(solution.two_sum(nums, target), expected)
-        print('Success: test_two_sum')
+        print("Success: test_two_sum")
 
 
 def main():
-    test = TestTwoSum()
+    test = TestCase()
     test.test_two_sum()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
