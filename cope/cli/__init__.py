738ddb6d378f15bb58a1e1aece525a547427eebd
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-02-02 08:33:14 +0000

    Feat: Add scoring CLI

--- cope/cli/__init__.py
+++ cope/cli/__init__.py
@@ -2,12 +2,13 @@
 
 import typer
 
-from cope.cli import auth, timetrial, timer
+from cope.cli import auth, highscore, leaderboard, timetrial
 
 app = typer.Typer()
 app.add_typer(auth.app, name="auth")
 app.add_typer(timetrial.app, name="timetrial")
-app.add_typer(timer.app, name="timer")
+app.add_typer(highscore.app, name="highscore")
+app.add_typer(leaderboard.app, name="leaderboard")
 
 if __name__ == "__main__":
     app()
---
+++ cope/cli/leaderboard.py
@@ -0,0 +1,19 @@
+"""Leaderboard commands."""
+
+import typer
+
+from cope.utils import ur
+
+app = typer.Typer()
+
+
+@app.callback(invoke_without_command=True)
+def leaderboard():
+    """Show the highscores."""
+    response = ur.leaderboard()
+    if not ur.has_error(response):
+        print(response.json())
+
+
+if __name__ == "__main__":
+    app()
