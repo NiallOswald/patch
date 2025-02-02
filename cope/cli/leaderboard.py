4aa6388aee90dec13ae012ef6b2b7fd7fcc0d08a
Akesh Mistry <akesh.mistry@caplin.com>
2025-02-02 10:24:38 +0000

    Feat: render leaderboard as a table

--- cope/cli/leaderboard.py
+++ cope/cli/leaderboard.py
@@ -1,6 +1,7 @@
 """Leaderboard commands."""
 
 import typer
+from tabulate import tabulate
 
 from cope.utils import ur
 
@@ -12,7 +13,9 @@ def leaderboard():
     """Show the highscores."""
     response = ur.leaderboard()
     if not ur.has_error(response):
-        print(response.json())
+        data = response.json()
+        headers = ["Username", "High Score"]
+        print(tabulate(data.items(), headers=headers))
 
 
 if __name__ == "__main__":
